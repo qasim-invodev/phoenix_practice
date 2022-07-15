c9dc7f1bd68f9aedd4b45bafe31689280b441bff
qasim-invodev <qasim.mehmood@invozone.com>
2022-07-15 14:39:51 +0500

    Plugs and Routes Done

--- hello/lib/hello_web/router.ex
+++ hello/lib/hello_web/router.ex
@@ -8,6 +8,7 @@ defmodule HelloWeb.Router do
     plug :put_root_layout, {HelloWeb.LayoutView, :root}
     plug :protect_from_forgery
     plug :put_secure_browser_headers
+    plug HelloWeb.Plugs.Locale, "en"
   end
 
   pipeline :api do
@@ -20,6 +21,18 @@ defmodule HelloWeb.Router do
     get "/", PageController, :index
     get "/hello", HelloController, :index
     get "/hello/:messenger", HelloController, :show
+    resources "/users", UserController do
+      resources "/posts", PostController
+    end
+    resources "/reviews", ReviewController
+  end
+
+  scope "/admin", HelloWeb.Admin, as: :admin do
+    pipe_through :browser
+
+    resources "/images",  ImageController
+    resources "/reviews", ReviewController
+    resources "/users",   UserController
   end
 
   # Other scopes may use custom stacks.
